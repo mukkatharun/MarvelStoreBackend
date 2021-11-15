335f0d4aab2ed53e0aff8f3ff6fd4a8b071f8b98
Tharun Mukka <tharunmukka1@gmail.com>
2021-11-15 10:51:33 -0800

    First commit

---
+++ src/main/java/com/example/marvelstore/MarvelStore/MarvelStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.marvelstore.MarvelStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelStoreApplication.class, args);
+	}
+
+}
